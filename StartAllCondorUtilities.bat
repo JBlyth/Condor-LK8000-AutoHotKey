ab1c49ff612fedb3c7c160eb0c0e3732c621f15d
JBlyth <jblyth@cheerful.com>
2022-09-21 18:03:42 +0800

    Add files via upload
    Starts LK8000 and CoTASA after Condor 2 task is started. Called from CoTaCo.

---
+++ StartAllCondorUtilities.bat
@@ -0,0 +1,15 @@
+@echo off
+rem cd """C:\Users\jblyt\OneDrive\Condor\CoTASAV3Beta2""
+rem Start "" "C:\Users\jblyt\OneDrive\Condor\CoTASAV3Beta2\CoTASA.exe"
+rem taskkill /F YourPhoneServer.exe
+rem pause
+cd """C:\Users\jblyt\Desktop""
+Start "" "CondorCom1Connection.lnk"
+Start "" "Condor LK8000 Control.lnk"
+Start "" "Condor Speak Rules.lnk"
+rem Start "" "Discord.lnk"
+rem Start "" "CoTaCo LK8000.lnk"
+rem Start "" "LK8000 Monitor 1.lnk"
+Start "" "C:\Users\jblyt\AppData\Local\Discord\Update.exe" --processStart Discord.exe
+Start "" "CONDOR NO RUDDER.lnk"
+rem Start "" "CONDOR NO RUDDER"
